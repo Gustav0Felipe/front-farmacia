4dec8fe247af2832b715b06b1ba8de17146b14a1
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2025-01-27 12:00:12 -0300

    Correção de links

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/Home'
 import CardCategoria from './components/cardcategoria/CardCategoria'
+import FormTema from './components/formcategoria/FormCategoria'
+import DeletarTema from './components/deletarcategoria/DeletarCategoria'
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/categoria/:id" element={<CardCategoria />} />
+          <Route path="/editarcategoria/:id" element={<FormTema />} />
+          <Route path="/deletarcategoria/:id" element={<DeletarTema />} />
         </Routes>
 
       <Footer></Footer>
